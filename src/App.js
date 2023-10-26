ee782c6a1609732c888007952adc48c5c3224a1e
Aigerim Sagimbayeva <aeka86@aol.com>
2023-10-26 22:05:37 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    // Code to run when the component is mounted
+    // You can fetch data, set up subscriptions, or perform other tasks here
+axios('https://api.randomuser.me/?na=US&results5')
+.then((response) => { setUsers(response.data.results)
+})
+    // For example, you can fetch data and update the state:
+    // fetchUsersData().then(data => {
+    //   setUsers(data);
+    // });
+  }, []); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      We'll be back!
     </div>
   );
 }

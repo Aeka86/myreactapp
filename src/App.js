34d1c57d6cec06377150f316fac3e4b859ae6f71
Aigerim Sagimbayeva <aeka86@aol.com>
2023-10-27 00:34:23 +0600

    some changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
 function App() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     // Code to run when the component is mounted
     // You can fetch data, set up subscriptions, or perform other tasks here
-axios('https://api.randomuser.me/?na=US&results5')
-.then((response) => { setUsers(response.data.results)
-})
+    axios("https://api.randomuser.me/?na=US&results5").then((response) => {
+      setUsers(response.data.results);
+    });
     // For example, you can fetch data and update the state:
     // fetchUsersData().then(data => {
     //   setUsers(data);
     // });
-  }, []); 
+  }, []);
 
-  return (
-    <div className="App">
-      We'll be back!
-    </div>
-  );
+  return <div className="App">We'll be back soon!</div>;
 }
 
 export default App;

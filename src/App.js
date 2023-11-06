48ce53b745429b374a0614bc70fb85cf68f4ff18
Aigerim Sagimbayeva <aeka86@aol.com>
2023-11-06 15:46:39 +0600

    added click event

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Loading } from "./Loading";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = () => {
     axios("https://api.randomuser.me/?na=US&results=5").then((response) => {
-      setUsers(response.data.results);
+      setUsers((prevUsers) => [...prevUsers, ...response.data.results]);
       setLoading(false);
     });
-    // For example, you can fetch data and update the state:
-    // fetchUsersData().then(data => {
-    //   setUsers(data);
-    // });
-  }, []);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    loadUsers();
+  };
 
   return (
     <div className="App">
-      { loading ? (
-        <p>Loading...</p>
+      {loading ? (
+        <Loading message="Welcome to my world!" />
       ) : (
-      users.map((user, index) => (
-        <div key={index}>
-          <p>Name: {user.name.first} {user.name.last}</p>
-          <p>{user.cell}</p>
-          <p>{user.email}</p>
-          <hr />
-        </div>
-      )))}
+        users.map((user, index) => (
+          <div key={index}>
+            <p>
+              Name: {user.name.first} {user.name.last}
+            </p>
+            <p>{user.cell}</p>
+            <p>{user.email}</p>
+            <hr />
+            <form onSubmit={handleSubmit}>
+              <input type="submit" value="load users" />
+            </form>
+          </div>
+        ))
+      )}
     </div>
   );
 }
